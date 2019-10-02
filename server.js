c2d37bf66869d17a10878ac8ac2745ca67475ccf
Julian <cusco2891@gmail.com>
2019-10-02 05:14:05 -0400

    server.js running

--- server.js
+++ server.js
@@ -2,3 +2,20 @@
 // require express
 // express-handlebars
 // mysql
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var port = process.env.PORT || 3030;
+var app = express();
+
+
+var handlebars = require('express-handlebars');
+
+app.engine('handlebars', handlebars({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+var routes = require('./controllers/burgers_controller.js');
+
+app.use('/', routes);
+
+app.listen(port);
