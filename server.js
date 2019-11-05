dcd9d8b2300b85540f811e1cdbfd78ea2e92f2e6
Julian <cusco2891@gmail.com>
2019-11-05 18:30:02 -0500

    check

--- server.js
+++ server.js
@@ -7,12 +7,12 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var expresshandlebars = require('express-handlebars');
-// var PORT = process.env.port || 3001;
+var PORT = process.env.port || 3001;
 
 var port = 3001;
 var app = express();
 
-// app.use(express.static(process.cwd() + '/public'));
+app.use(express.static(process.cwd() + '/public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(methodOverride('method'));
 
@@ -25,5 +25,5 @@ var routes = require('./controllers/burgers_controller.js');
 app.use('/', routes);
 
 app.listen(port, function() {
-    console.log("App listening on PORT:", port);
+    console.log("Server listening on PORT:", port);
 });
